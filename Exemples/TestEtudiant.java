ed6d65adaa2f0c861fbc36073d9fdddc03d9115a
Akil Elkamel <akil_elkamel@yahoo.fr>
2026-03-02 17:07:03 +0100

    Add TestEtudiant class to test Etudiant instances
    This class creates instances of Etudiant and displays one.

---
+++ Exemples/TestEtudiant.java
@@ -0,0 +1,12 @@
+
+public class TestEtudiant {
+    
+    public static void main(String[] args) {
+        Etudiant e1 = new Etudiant(123, "Ali", "MP3I", 1);
+        Etudiant e2 = new Etudiant(456, "Ahmed", "MP3I", 1);
+        Etudiant e3 = new Etudiant(789, "Nour Elhouda", "MP3I", 1);
+        Etudiant e4 = new Etudiant(951, "Hajer", "MP3I", 1);
+        
+        e1.afficher();
+    }
+}
