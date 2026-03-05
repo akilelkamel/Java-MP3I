82688ac7d6038d49fdcbe61c5062ad1f9bcff56e
Akil Elkamel <akil_elkamel@yahoo.fr>
2026-03-05 11:41:36 +0100

    Add TestPoint class with main method for Point

---
+++ Exemples/TestPoint.java
@@ -0,0 +1,11 @@
+
+public class TestPoint {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(3, 4);
+        p1.afficher();
+
+        Point p2 = new Point();
+        p2.afficher();
+    }
+}
